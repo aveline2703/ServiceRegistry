b4cc42deedafca93bcc5c266c2b78890ea858dbf
Pratik <pratik.pj48@gmail.com>
2023-01-11 17:25:54 +0530

    create service registry

---
+++ src/test/java/com/pratik/www/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.www.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
